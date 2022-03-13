5532959aa7aecc6981fb6c2f5a4663c9d10113ae
Olivier Laurent <olivier.ar.laurent@gmail.com>
2022-03-14 00:04:48 +0100

    Split and improve converter

--- s3ts/converter.py
+++ s3ts/converter.py
@@ -4,14 +4,25 @@ from pydub import AudioSegment
 
 
 class Converter:
-    def __init__(self) -> None:
-        pass
+    def __init__(self, frame_rate: int = 16000, mono: bool = True) -> None:
+        self.frame_rate = frame_rate
+        self.mono = mono
 
-    def convert(self, src_path: Path, dst_path: Path):
-        src = "transcript.mp3"
-        dst = "test.wav"
+    def std_convert(self, src_path: Path, dst_path: Path):
+        sound = self.read(src_path=src_path)
+        sound = self.set_framerate(sound=sound)
+        if self.mono:
+            sound = self.to_mono(sound=sound)
+        self.export(sound, dst_path=dst_path)
 
-        # convert wav to mp3
-        sound = AudioSegment.from_mp3(src)
-        sound.set_channels(1)
-        sound.export(dst, format="wav")
+    def read(self, src_path: Path):
+        return AudioSegment.from_mp3(src_path)
+
+    def set_framerate(self, sound):
+        return sound.set_frame_rate(self.frame_rate)
+
+    def to_mono(self, sound):
+        return sound.set_channels(1)
+
+    def export(self, sound, dst_path: Path):
+        sound.export(dst_path, format="wav")
