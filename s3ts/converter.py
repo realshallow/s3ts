f657e32bc4caef1269536c1fd8dfe975fdc034cc
Olivier Laurent <olivier.ar.laurent@gmail.com>
2022-03-16 15:36:56 +0100

    Convert can treat multiple files at the same time

--- s3ts/converter.py
+++ s3ts/converter.py
@@ -8,12 +8,28 @@ class Converter:
         self.frame_rate = frame_rate
         self.mono = mono
 
-    def std_convert(self, src_path: Path, dst_path: Path):
+    def std_convert(self, src_path: Path, dst_path: Path, verbose: bool = True):
+        if src_path.is_dir() and dst_path.is_dir():
+            source_files = list(src_path.glob("*"))
+            if verbose:
+                print("Found", len(source_files), "audio files to process.")
+            for file_path in source_files:
+                print(file_path)
+                self.process_file(file_path, Path(dst_path, file_path.stem))
+
+        elif src_path.is_file() and dst_path.is_file():
+            self.process_file(src_path, dst_path)
+        else:
+            raise ValueError(
+                "The source and destination paths should be both folders or both files."
+            )
+
+    def process_file(self, src_path, dst_path):
         sound = self.read(src_path=src_path)
         sound = self.set_framerate(sound=sound)
         if self.mono:
             sound = self.to_mono(sound=sound)
-        self.export(sound, dst_path=dst_path)
+        self.export(sound, dst_path=Path(str(dst_path.with_suffix("")) + ".wav"))
 
     def read(self, src_path: Path):
         return AudioSegment.from_mp3(src_path)
